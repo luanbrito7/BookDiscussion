01be75b139905d5dee27e110d2a4ebe3c4e8b057
luanbrito7 <lab7@cin.ufpe.br>
2021-04-12 17:53:44 -0300

    remove book.equals override method

--- app/src/main/java/com/example/bookdiscussion/Book.kt
+++ app/src/main/java/com/example/bookdiscussion/Book.kt
@@ -4,8 +4,4 @@ data class Book (
         val title:String, val description:String,
         val image_url:String, val author:String, val id:String
     ){
-
-    fun equals(other: Book?): Boolean {
-        return id == other?.id
-    }
 }
