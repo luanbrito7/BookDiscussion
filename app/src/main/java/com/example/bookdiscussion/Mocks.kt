1f66c75336cba9cea7952638699fbd607e694390
luanbrito7 <lab7@cin.ufpe.br>
2021-04-17 11:28:54 -0300

    fix mocks

--- app/src/main/java/com/example/bookdiscussion/Mocks.kt
+++ app/src/main/java/com/example/bookdiscussion/Mocks.kt
@@ -3,31 +3,31 @@ package com.example.bookdiscussion
 object Mocks {
     val books = arrayOf(
         Book(
+            "1",
             "O mal estar na cultura",
             "Sigmund Freud Revisão técnica e prefácio de Márcio Seligmann-SilvaEnsaio biobibliográfico de Paulo Endo e Edson SousaTradução de Renato Zwick Perguntando-se sobre os critérios ? ao seu ver equivocados ? usados pelos seres humanos para eleger os valores da vida que lhes são caros e assim traçar caminhos na busca pela felicidade, Sigmund Freud (1856-1939) inicia uma reflexão sobre a origem da necessidade do sentimento religioso no homem. Recuperando ideias de seus textos anteriores, ele compõe O mal-estar na cultura (escrito em 1929 e publicado em 1930), um dos mais perturbadores ensaios jamais escritos no que diz respeito ao desenvolvimento cultural da humanidade. Ao investigar por que o ser humano é tão pouco dotado para ser e permanecer feliz, Freud revela que um dos principais e invencíveis obstáculos à felicidade é a constituição psíquica do homem. Ele examina de perto ? lançando mão de ferramentas psicanalíticas ? o processo de desenvolvimento cultural necessário para que as pessoas possam viver em sociedade. A conclusão é a de que não só a civilização, mas a própria cultura humana implicam uma diminuição na felicidade dos indivíduos, tendo como subproduto um alienável e generalizado sentimento de culpa.",
             "https://images-na.ssl-images-amazon.com/images/I/81VeHdd811L.jpg",
             "Freud",
-            "1",
             false,
             false,
             false
         ),
         Book(
+            "2",
             "O mal estar na civilização",
             "Escrito às vésperas do colapso da Bolsa de Valores de Nova York (1929) e publicado em Viena no ano seguinte, O mal-estar na civilização é uma penetrante investigação sobre as origens da infelicidade, sobre o conflito entre indivíduo e sociedade e suas diferentes configurações na vida civilizada. Este clássico da antropologia e da sociologia também constitui, nas palavras do historiador Peter Gay, “uma teoria psicanalítica da política”. Na tradução de Paulo César de Souza, que preserva a exatidão conceitual e toda a dimensão literária da prosa do criador da psicanálise, o livro proporciona um verdadeiro mergulho na teoria freudiana da cultura, segundo a qual civilização e sexualidade coexistem de modo sempre conflituoso. A partir dos fundamentos biológicos da libido e da agressividade, Freud demonstra que a repressão e a sublimação dos instintos sexuais, bem como sua canalização para o mundo do trabalho, constituem as principais causas das doenças psíquicas de nossa época.",
             "https://images-na.ssl-images-amazon.com/images/I/41GN+XNvFoL._SX322_BO1,204,203,200_.jpg",
             "Freud",
-            "2",
             false,
             false,
             false
         ),
         Book(
+            "3",
             "Totem e Tabu",
             "A série Grandes Ideias lança agora um dos textos mais conhecidos e de profunda abrangência já escritos por Sigmund Freud: Totem e tabu, também com tradução de Paulo César de Souza. O subtítulo “Algumas concordâncias entre a vida dos homens primitivos e dos neuróticos” não chega a dar ideia da riqueza dos temas que aborda, pois os quatro ensaios que o compõem tratam da origem da religião e da moralidade, ou seja, da própria civilização. Baseando-se em estudos de antropologia, biologia e história, Freud lança a conjectura de que o ato fundador da sociedade humana foi o assassinato do pai da horda primitiva pelos próprios filhos. Totem e tabu foi a primeira aplicação da psicanálise a questões de psicologia social. A edição deste fundamental ensaio de Freud é lançada após o enorme sucesso de vendas de O mal-estar na civilização, uma penetrante investigação sobre as origens da infelicidade, sobre o conflito entre indivíduo e sociedade e suas diferentes configurações na vida civilizada.",
             "https://images-na.ssl-images-amazon.com/images/I/31pED+QsYxL._SX323_BO1,204,203,200_.jpg",
             "Freud",
-            "3",
             false,
             false,
             false
