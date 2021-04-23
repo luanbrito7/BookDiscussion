a055fd372967c8ab000ecd48e4a3bf3a5adee4fb
Eden Ernandes <eden.santana@neurotech.com.br>
2021-04-23 10:33:59 -0300

    Add addToShelf and likeBook features

--- app/src/main/java/com/example/bookdiscussion/BookActivity.kt
+++ app/src/main/java/com/example/bookdiscussion/BookActivity.kt
@@ -8,10 +8,13 @@ import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import com.example.bookdiscussion.databinding.ActivityBookBinding
 import androidx.activity.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+
 
 class  BookActivity : AppCompatActivity() {
     private lateinit var binding : ActivityBookBinding
@@ -36,24 +39,48 @@ class  BookActivity : AppCompatActivity() {
         binding.bookDescription.text = description
 
         val likeButton: Button = findViewById(R.id.likeButton)
-        likeButton.setOnClickListener { likeBook("xalala") }
+        likeButton.setOnClickListener {
+            if (id != null) {
+                likeBook(id)
+            }
+        }
 
         val shelfButton: Button = findViewById(R.id.addShelfButton)
         shelfButton.setOnClickListener {
             if (id != null) {
-                addBookToShelf(id)
+                addBookToShelf(id, title, author, description)
             }
         }
     }
 
     fun likeBook(id: String?) {
+        var liveData: MutableLiveData<Book>?
+
+        if (id != null) {
+            liveData = viewModel.getBookById(id)
+            liveData.observe(
+                this,
+                Observer { b ->
+                    b.liked = true
+                    viewModel.update(b)
+                }
+            )
+        }
+
         Toast.makeText(applicationContext, "Book added to your liked books!", LENGTH_SHORT).show()
     }
 
-    fun addBookToShelf(id: String) {
-        Log.d("Id! ", id)
-        var book: Any = viewModel.getBookById(id)
-        Log.d("AQUI! ", book.toString())
+    /*
+    * TODO
+    * Ajustar a recuperacão da imagem
+    * */
+    fun addBookToShelf(id: String, title: String?, author: String?, description: String?) {
+        var book: Book?
+        if (title != null && author != null && description != null) {
+            book = Book(id, title, description, "", author, false, false, false, 0, false)
+            viewModel.insert(book)
+        }
+
         Toast.makeText(applicationContext, "Book added to your shelf!", LENGTH_SHORT).show()
     }
 }
