ba06367836aa5fb855317d0be126e461a8de166c
Eden Ernandes <eden.santana@neurotech.com.br>
2021-04-24 00:05:26 -0300

    Add ratingBar

--- app/src/main/java/com/example/bookdiscussion/activities/BookActivity.kt
+++ app/src/main/java/com/example/bookdiscussion/activities/BookActivity.kt
@@ -2,8 +2,11 @@ package com.example.bookdiscussion.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.RatingBar
 import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import android.widget.Toast.LENGTH_SHORT
 import com.example.bookdiscussion.databinding.ActivityBookBinding
 import androidx.activity.viewModels
@@ -31,11 +34,13 @@ class  BookActivity : AppCompatActivity() {
         val title = intent.getStringExtra("title")
         val author = intent.getStringExtra("author")
         val description = intent.getStringExtra("description")
+        val rate = intent.getFloatExtra("rate", 0F)
 
        // binding.bookId.text = id
         binding.bookTitle.text = title
         binding.bookAuthor.text = author
         binding.bookDescription.text = description
+        binding.ratingBar.rating = rate
 
         val likeButton: Button = findViewById(R.id.likeButton)
         likeButton.setOnClickListener {
@@ -50,6 +55,33 @@ class  BookActivity : AppCompatActivity() {
                 addBookToShelf(id, title, author, description)
             }
         }
+
+        val ratingBar: RatingBar = findViewById(R.id.ratingBar)
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+
+            var liveData: MutableLiveData<Book>?
+
+            if (id != null) {
+                liveData = viewModel.getBookById(id)
+                liveData.observe(
+                    this,
+                    Observer { b ->
+                        if (b != null) {
+                            b.rate = rating.toInt()
+                            viewModel.update(b)
+                            Toast.makeText(applicationContext, "You rated this book as ${rating.toInt()} stars", LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(applicationContext, "You must add to your shelf before rating", LENGTH_LONG).show()
+                        }
+
+                    }
+                )
+            }
+
+
+
+        }
+
     }
 
     fun likeBook(id: String?) {
@@ -94,3 +126,4 @@ class  BookActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, "Book added to your shelf!", LENGTH_SHORT).show()
     }
 }
+
--- app/src/main/java/com/example/bookdiscussion/models/Book.kt
+++ app/src/main/java/com/example/bookdiscussion/models/Book.kt
@@ -5,17 +5,17 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "books")
 data class Book (
-        @PrimaryKey
+    @PrimaryKey
     val id:String,
-        val title:String,
-        val description:String,
-        val image_url:String,
-        val author:String,
-        val reading: Boolean,
-        val read: Boolean,
-        val wantToRead: Boolean,
-        val rate: Int,
-        var liked: Boolean
+    val title:String,
+    val description:String,
+    val image_url:String,
+    val author:String,
+    val reading: Boolean,
+    val read: Boolean,
+    val wantToRead: Boolean,
+    var rate: Int,
+    var liked: Boolean
 ){
 
     override fun toString(): String {
@@ -23,9 +23,10 @@ data class Book (
                 getStringAttr("title", title) +
                 getStringAttr("desc", description) +
                 getStringAttr("image_url", image_url) +
-                getStringAttr("author", author)
+                getStringAttr("rate", rate)
 
     }
+
 
     fun getStringAttr(label: String, value: Any) : String {
         return label + ": " + value.toString() + "\n"
